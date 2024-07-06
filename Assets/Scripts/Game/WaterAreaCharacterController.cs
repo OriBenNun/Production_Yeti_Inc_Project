4846dd0a01c43ddc3e11be55344a0b72ddf05a7d
BmanStudio <oribennoon@gmail.com>
2024-07-06 18:41:14 +0300

    did renaming after merging with Amit's work

--- Assets/Scripts/Game/WaterAreaCharacterController.cs
+++ Assets/Scripts/Game/WaterAreaCharacterController.cs
@@ -1,9 +1,25 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game
 {
     public class WaterAreaCharacterController : MonoBehaviour
     {
-    
+        [SerializeField] private List<LaneManager> lanes;
+        // [SerializeField] private 
+        
+        private void Awake()
+        {
+            foreach (var lane in lanes)
+            {
+                lane.OnLaneClicked += HandleOnLaneClicked;
+            }
+        }
+
+        private void HandleOnLaneClicked()
+        {
+            
+        }
     }
 }
