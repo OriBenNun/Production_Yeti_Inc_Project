061efc02d1756970c673ecd20c0a30efb6f24b8e
Amit Breiman <crimson.breiman@gmail.com>
2024-08-17 11:35:06 +0300

    1

--- Assets/Scripts/Game/Shop/ShopItem.cs
+++ Assets/Scripts/Game/Shop/ShopItem.cs
@@ -1,23 +1,153 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ShopItem : MonoBehaviour
+public class ShopItem : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private Image _itemImage;
     [SerializeField] private ShopItemData _itemData;
     public ShopItemData Data { get { return _itemData; } }
 
+
+    [SerializeField] private Color NormalColor;
+    [SerializeField] private Color BlockedColor;
+
+
+    private ShopManager _shopManager;
     private bool _isPurchasable = true;
     private bool _isAvailable = true;
 
     
-    
+    public void Init(ShopManager shopManager)
+    {
+        _shopManager = shopManager;
+        UpdateState();
+    }
+
     public void SetPurchasable(bool isPurchasable)
     {
         _isPurchasable = isPurchasable;
+
+        if (_isPurchasable)
+        {
+            _itemImage.color = NormalColor;
+        }
+        else
+        {
+            _itemImage.color = BlockedColor;
+        }
     }
     public void SetAvailable(bool isAvailable)
     {
         _isAvailable = isAvailable;
         gameObject.SetActive(isAvailable);
+
+        
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!_isPurchasable) return;
+
+
+        throw new System.NotImplementedException();
     }
 
+    public void UpdateState()
+    {
+        
+
+        switch (Data.Enum)
+        {
+            case ItemEnums.Wood:
+                UpdateWood();
+                break;
+            case ItemEnums.WoodBundle:
+                UpdateWoodBundle();
+                break;
+            case ItemEnums.Net:
+                UpdateNet();
+                break;
+            case ItemEnums.Helmet:
+                UpdateHelmet();
+                break;
+            case ItemEnums.MetalRam:
+                UpdateRam();
+                break;
+        }
+    }
+
+    private void UpdateWood()
+    {
+        SetAvailable(true);
+
+        if (TempDataHolder.CurrentCurrency >= Data.Price && 
+            TempDataHolder.MaxLife - TempDataHolder.CurrentLife > 0)
+        {
+            SetPurchasable(true);
+        }
+        else
+        {
+            SetPurchasable(false);
+        }
+    }
+
+    private void UpdateWoodBundle()
+    {
+        SetAvailable(true);
+
+        if (TempDataHolder.CurrentCurrency >= Data.Price &&
+            TempDataHolder.MaxLife - TempDataHolder.CurrentLife > 1)
+        {
+            SetPurchasable(true);
+        }
+        else
+        {
+            SetPurchasable(false);
+        }
+    }
+
+    private void UpdateRam()
+    {
+        SetAvailable(true);
+
+        if (TempDataHolder.CurrentCurrency >= Data.Price &&
+            !TempDataHolder.HasRam)
+        {
+            SetPurchasable(true);
+        }
+        else
+        {
+            SetPurchasable(false);
+        }
+    }
+
+    private void UpdateHelmet()
+    {
+        SetAvailable(true);
+
+        if (TempDataHolder.CurrentCurrency >= Data.Price &&
+            !TempDataHolder.HasHelmet)
+        {
+            SetPurchasable(true);
+        }
+        else
+        {
+            SetPurchasable(false);
+        }
+    }
+
+    private void UpdateNet()
+    {
+        SetAvailable(true);
+
+        if (TempDataHolder.CurrentCurrency >= Data.Price &&
+             !TempDataHolder.HasNet)
+        {
+            SetPurchasable(true);
+        }
+        else
+        {
+            SetPurchasable(false);
+        }
+    }
 }
--- Assets/Scripts/Game/Shop/ShopManager.cs
+++ Assets/Scripts/Game/Shop/ShopManager.cs
@@ -3,5 +3,23 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
+
+    [SerializeField] private CurrencyDisplay _currencyDisplay;
     [SerializeField] private List<ShopItem> _items;
+
+    private void Awake()
+    {
+        TempDataHolder.Init();
+
+        foreach (ShopItem item in _items)
+        {
+            item.Init(this);
+        }
+
+        _currencyDisplay.UpdateCurrencyText(CurrentCurrency);
+    }
+
+    // Replace this with the way to get and set the currency in the game.
+    public int CurrentCurrency { get { return TempDataHolder.CurrentCurrency; } }
+
 }
